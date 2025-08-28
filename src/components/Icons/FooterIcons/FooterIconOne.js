867f4e6f150e10f1bc3103f10e079847cc755ee8
oleg voronin <oleg83voronin@gmail.com>
2025-08-28 19:56:11 +0300

    add icons

---
+++ src/components/Icons/FooterIcons/FooterIconOne.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function FooterIconOne() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="8.07"
+      height="14.72"
+      fill="none"
+      viewBox="0 0 8.07 14.72"
+    >
+      <path
+        fill="#b2b2b2"
+        fillOpacity="1"
+        fillRule="nonzero"
+        d="M7.541 8.28l.419-2.664H5.343V3.887c0-.729.366-1.439 1.538-1.439h1.19V.18S6.99 0 5.958 0C3.804 0 2.395 1.276 2.395 3.586v2.03H0V8.28h2.395v6.44h2.948V8.28h2.198z"
+      ></path>
+    </svg>
+  );
+}
+
+export default FooterIconOne;
---
+++ src/components/Icons/ProductsIcon/FilterIcon.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function FilterIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="25"
+      fill="none"
+      viewBox="0 0 20 25"
+    >
+      <g fill="#000">
+        <path d="M21 6H3a1 1 0 100 2h18a1 1 0 100-2zM15 11H3a1 1 0 100 2h12a1 1 0 100-2zM9 16H3a1 1 0 100 2h6a1 1 0 100-2z"></path>
+      </g>
+    </svg>
+  );
+}
+
+export default FilterIcon;
